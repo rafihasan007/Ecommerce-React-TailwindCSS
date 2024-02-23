65e98acfff3a370c42b12f1c53b3462619df501a
rafihasan007 <r.rafihasan2@gmail.com>
2024-02-23 14:58:43 +0600

    Saving the project in github

---
+++ React Project/src/hooks/useLocalStorage.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+function useLocalStorage() {
+  const [account, setAccount] = useState({});
+  const [isSignIn, setIsSignIn] = useState(false);
+  
+  const item = "ACCOUNT";
+
+  useEffect(() => {
+    const accountStorage = localStorage.getItem(item);
+
+    if (accountStorage !== null) {
+      setAccount(JSON.parse(accountStorage));
+      setIsSignIn(true);
+    } else {
+      localStorage.setItem(item, JSON.stringify(account));
+    }
+  }, []);
+
+  const signIn = (newAccount = account) => {
+    localStorage.setItem(item, JSON.stringify(newAccount));
+
+    setAccount(newAccount);
+    setIsSignIn(true);
+  }
+
+  const signOut = () => {
+    localStorage.setItem(item, JSON.stringify({}));
+
+    setAccount({});
+    setIsSignIn(false);
+  }
+
+  return {
+    account,
+    signIn,
+    signOut,
+    isSignIn
+  }
+}
+
+export { useLocalStorage }
